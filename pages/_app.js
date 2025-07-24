8f665e3669223ba3a3b371cc7a42d2fc7161d080
TerribleSpeller <54887256+TerribleSpeller@users.noreply.github.com>
2025-07-24 13:22:48 +0700

    Base

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import "@/styles/globals.css";
+import { useRouter } from 'next/router';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  
   return <Component {...pageProps} />;
 }
